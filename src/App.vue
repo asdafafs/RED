d90e6167a910c3da0567405f0d41e0bdd3ca6db4
Anton <anton_vasilev00@mail.ru>
2023-11-04 20:55:02 +0300

    Revert "Добавил тестово три шедулера"
    This reverts commit d60209a319f1659b367765ee41db93a9845b364c.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,24 @@
 <template>
   <div id="app">
-
-    Пример Devextreme
-    <div><DevextremeExample></DevextremeExample></div>
-
-    Пример Jqx
-
-    <div><JqxScheduler></JqxScheduler></div>
-    Пример Syncfusion
-    <div><SyncfusionScheduler></SyncfusionScheduler></div>
+    <div>{{ users }}</div>
   </div>
 </template>
 
 <script>
-import DevextremeExample from "@/components/DevextremeExample.vue";
-import JqxScheduler from "@/components/JqxScheduler.vue";
-import SyncfusionScheduler from "@/components/SyncfusionScheduler.vue";
-
 
 export default {
-  components: {SyncfusionScheduler, JqxScheduler, DevextremeExample, },
-
-//  data: () => ({
-//    users: ''
-    //  }),
-//  async mounted() {
-//    await this.getUsers();
-//  },
-//  methods: {
-//    async getUsers() {
-//      const response = await fetch("https://localhost:7012/users");
-//      this.users = await response.json();
-//    }
-//  },
+  data: () => ({
+    users: ''
+  }),
+  async mounted() {
+    await this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      const response = await fetch("https://localhost:7012/users");
+      this.users = await response.json();
+    }
+  },
 }
 </script>
 
@@ -57,5 +43,4 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
-
 </style>
