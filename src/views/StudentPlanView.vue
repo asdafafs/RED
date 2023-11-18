6f79ae66b495787f517592ee0ee99fc72b8db7fb
Anton <anton_vasilev00@mail.ru>
2023-11-19 01:12:15 +0300

    Вынес выбор календарей в отдельный элемент и удалил старый календарь

--- src/views/StudentPlanView.vue
+++ src/views/StudentPlanView.vue
@@ -7,7 +7,6 @@
            <v-img
                src="https://cdn.vuetifyjs.com/images/john.jpg"
                alt="John">
-
            </v-img>
         </v-avatar>
       </v-col>
@@ -21,67 +20,31 @@
       </v-spacer>
       <v-col cols="1">
         <v-btn text class="grey--text btn pa-0" width="100%"
-               @click="changeButtonMenuState(0);">
+               @click="changeButtonMenuState(0); $router.push('/testPlan/mainCal').catch(err => {})">
           <span :class="{'uno': isButtonMenuPressed[0],}">Календарь</span>
         </v-btn>
         <div v-show="isButtonMenuPressed[0]" class="button-line"></div>
       </v-col>
       <v-col cols="1">
         <v-btn text class="grey--text btn pa-0" width="100%"
-               @click="changeButtonMenuState(1);">
+               @click="changeButtonMenuState(1); $router.push('/testPlan/progress').catch(err => {})">
          <span :class="{'uno': isButtonMenuPressed[1],}">Прогресс</span>
         </v-btn>
         <div v-show="isButtonMenuPressed[1]" class="button-line"></div>
       </v-col>
     </v-row>
     <hr>
-    <v-row class="px-12">
-      <v-col cols="12">
-        <div class="text-h6 font-weight-bold">Мое расписание</div>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex mt-0 ga-3 " no-gutters>
-      <v-col cols="1">
-        <v-btn text class="black--text btn pa-0" width="100%"
-               :class="{'custom-bg': isButtonPressed[0],}"
-               @click="changeButtonState(0);
-               $router.push('/testPlan/testDay').catch(err => {})"
-        >
-          <span :class="{ 'custom_text':isButtonPressed[0]}">День</span>
-        </v-btn>
-      </v-col >
-      <v-col cols="1">
-        <v-btn text  class="black--text btn pa-0" width="100%"
-               :class="{'custom-bg': isButtonPressed[1]}"
-               @click="changeButtonState(1);
-               $router.push('/testPlan/testWeek').catch(err => {})">
-          <span :class="{ 'custom_text':isButtonPressed[1]}">Неделя</span>
-        </v-btn>
-      </v-col>
-      <v-col cols="1">
-        <v-btn text class="black--text btn pa-0" width="100%"
-               :class="{'custom-bg': isButtonPressed[2]}"
-               @click="changeButtonState(2);
-               $router.push('/testPlan/testMonth').catch(err => {})">
-          <span :class="{ 'custom_text':isButtonPressed[2]}">Месяц</span>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <router-view></router-view>
-      </v-col>
-    </v-row>
+    <router-view></router-view>
   </v-container>
 </template>
 <script>
+
 export default {
   components: {},
   data: () => ({
     name: 'Сергей',
     groupId: 1,
     isButtonMenuPressed: [false, false,],
-    isButtonPressed: [false, false, false]
   }),
   methods:{
     changeButtonMenuState(index) {
@@ -91,13 +54,6 @@ export default {
       this.$set(this.isButtonMenuPressed, index, true);
       this.lastPressedIndex = index;
     },
-    changeButtonState(index){
-      if (this.lastPressedIndex !== -1) {
-        this.$set(this.isButtonPressed, this.lastPressedIndex, false);
-      }
-      this.$set(this.isButtonPressed, index, true);
-      this.lastPressedIndex = index;
-    }
   }
 }
 
