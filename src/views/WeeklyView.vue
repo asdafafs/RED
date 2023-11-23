8e0df128e5e525c5291d83ff29ff592384281b21
Anton <anton_vasilev00@mail.ru>
2023-11-23 15:55:45 +0300

    Убрал лишний массив событий

--- src/views/WeeklyView.vue
+++ src/views/WeeklyView.vue
@@ -118,53 +118,6 @@ import {mapState} from "vuex";
       test: false,
       dateMonday: moment().subtract(0, 'weeks').startOf('isoWeek').format('DD'),
       dateSunday: moment().subtract(0, 'weeks').endOf('isoWeek').format('DD'),
-      // events: [
-      //   {
-      //   name: 'Лекция',
-      //   start: new Date(2023, 10, 13, 3, 0),
-      //   end: new Date(2023, 10, 13, 5, 0),
-      //   startTime: '11:00',
-      //   type: 'lecture',
-      //   timed: true,
-      //   teacher: 'Каминский С.В.'
-      // },
-      // {
-      //   name: 'Вождение',
-      //   start: new Date(2023, 10, 15, 3, 0),
-      //   end: new Date(2023, 10, 15, 5, 0),
-      //   startTime: '14:00',
-      //   type: 'practice',
-      //   timed: true,
-      //   teacher: 'Каминский С.В.'
-      // },
-      // {
-      //   name: 'Лекция',
-      //   start: new Date(2023, 10, 11, 3, 0),
-      //   end: new Date(2023, 10, 11, 5, 0),
-      //   startTime: '14:00',
-      //   type: 'lecture',
-      //   timed: true,
-      //   teacher: 'Каминский С.В.'
-      // },
-      //           {
-      //   name: 'Вождение',
-      //   start: new Date(2023, 10, 23, 3, 0),
-      //   end: new Date(2023, 10, 23, 5, 0),
-      //   startTime: '14:00',
-      //   type: 'practice',
-      //   timed: true,
-      //   teacher: 'Каминский С.В.'
-      // },
-      // {
-      //   name: 'Лекция',
-      //   start: new Date(2023, 10, 24, 3, 0),
-      //   end: new Date(2023, 10, 24, 5, 0),
-      //   startTime: '14:00',
-      //   type: 'lecture',
-      //   timed: true,
-      //   teacher: 'Каминский С.В.'
-      // }
-      // ],
     }),
     methods: {
       updateRange () {},
