e188117e4f503a342bec0da1053ea057a655887f
Anton <anton_vasilev00@mail.ru>
2023-11-26 03:28:32 +0300

    Небольшой рефакторинг

---
+++ src/components/AppBar/DesktopLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="pa-0" fluid style="height: 100%;">
+    <v-row no-gutters align="center" style="height: 100%;">
+      <v-col cols=12 class="d-flex justify-center align-center">
+        <LogoRed></LogoRed>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import LogoRed from "@/components/logos/LogoRed.vue"
+
+export default {
+  name: 'DesktopLogo',
+  components: {LogoRed}
+}
+</script>
