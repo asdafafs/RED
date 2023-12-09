466bbe5c9527d3b322cca2727049f193c96e00f8
Anton <anton_vasilev00@mail.ru>
2023-12-09 18:10:50 +0300

    Переделал отображение результатов запросов

--- src/services/CoursesRequest.js
+++ src/services/CoursesRequest.js
@@ -1,23 +1,23 @@
-import {HTTPService} from "@/services/HTTPService";
+import HTTPService from "@/services/HTTPService";
 
 export default class CoursesRequest extends HTTPService {
     constructor() {
         super("");
     }
 
-//     getCourses(){
-//         return this.get("")
-//     }
-//
-//     postCourse(code){
-//         return this.post("", code)
-//     }
-//
-//     putCourse(code){
-//         return this.put("", code)
-//     }
-//
-//     deleteCourse(code){
-//         return this.delete("", {id : code})
-//     }
+    getCourses() {
+        return this.get("")
+    }
+
+    postCourse(code) {
+        return this.post("", code)
+    }
+
+    putCourse(code) {
+        return this.put("", code)
+    }
+
+    deleteCourse(id) {
+        return this.delete("" + id)
+    }
 }
