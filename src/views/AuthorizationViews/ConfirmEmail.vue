7728fda70f6963a3fb955bdb215cd22cd780cbca
sakhn <sakhnikarseni@mail.ru>
2024-01-04 23:14:34 +0300

    # Блядь я не понимаю как это работает

--- src/views/AuthorizationViews/ConfirmEmail.vue
+++ src/views/AuthorizationViews/ConfirmEmail.vue
@@ -6,21 +6,28 @@
 
 <script>
 import IdentityRequest from "@/services/IdentityRequest";
+import store from "@/store";
 
-export default  {
+export default {
   async mounted() {
     const userId = this.$route.query.userId
     const code = this.$route.query.code
 
     await this.confirmEmail(userId, code)
   },
-  methods:{
-    async confirmEmail(userId, code ){
+  methods: {
+    async confirmEmail(userId, code) {
       const identity = new IdentityRequest()
       await identity.postEmail({userId, code}).then(() => {
-          identity.getIdentity();
-          this.$router.push('/schedule/lessons')
-        })
+        identity.getIdentity()
+            .then(async (x) => {
+              await store.dispatch('GET_CURRENT_USER', x)
+              this.isDataLoaded = true
+
+              if (this.$route.path === '/')
+                await this.$router.push('/schedule/lessons')
+            })
+      })
 
     }
   }
--- src/views/AuthorizationViews/MailAuthorization.vue
+++ src/views/AuthorizationViews/MailAuthorization.vue
@@ -8,6 +8,7 @@
             <v-card-subtitle class="black--text">
               Для продолжения работы в RED: Расписание, пожалуйста, авторизуйтесь через электронную почту.
             </v-card-subtitle>
+            <v-alert v-if="message" type="success"></v-alert>
             <LogoRed class="pos"></LogoRed>
             <v-card-text class="pb-0 ">
               <v-text-field
@@ -77,9 +78,16 @@ export default {
       required: value => !!value || 'Введите пароль',
       min: v => v.length >= 8 || 'Минимум 8 символов',
     },
-    wrongAuth: false
+    wrongAuth: false,
+    message: null
   }),
-
+  created() {
+    console.log('MailAuthorization')
+    const isConfirmRequired = this.$route.query.isConfirmRequired;
+    if (isConfirmRequired) {
+      this.message = "На указанный адрес электронной почты было отправлено письмо с подтверждением";
+    }
+  },
   methods: {
     async login(body) {
       const login = new IdentityRequest()
