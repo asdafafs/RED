94a3705887fe960899875db381087d66a43bdf63
sakhn <sakhnikarseni@mail.ru>
2024-03-14 20:55:49 +0300

    # Тест

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Путь к файлу .env
+env_file=".env"
+
+# Проверка существования файла .env
+if [ ! -f "$env_file" ]; then
+    echo "Ошибка: Файл .env не найден."
+    exit 1
+fi
+
+sed -i "s/^FRONTEND_VERSION=.*/FRONTEND_VERSION=$FRONTEND_VERSION/" "$env_file"
+#docker compose down red-schedule-frontend
+#docker compose up -d
