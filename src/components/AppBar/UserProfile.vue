efccf286475fd1a16f906435fce406260b31694e
sakhn <sakhnikarseni@mail.ru>
2024-03-14 11:27:37 +0300

    # Фикс логина в вк

--- src/components/AppBar/UserProfile.vue
+++ src/components/AppBar/UserProfile.vue
@@ -49,7 +49,7 @@ export default {
 
     async linkVk() {
       const clientId = '51785736'
-      const redirectUri = `https://anton.techbeaver.ru/post-login`
+      const redirectUri = `https://red.techbeaver.ru/post-login`
       const display = 'popup'
       const responseType = 'code'
       window.location.replace(`https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&display=${display}&response_type=${responseType}`)
