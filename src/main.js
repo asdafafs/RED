4c6b1442c6a443d4d3b6c85974f388f79712bb99
baskakov <playingthefiend2@mail.ru>
2024-04-22 23:55:12 +0300

    #46 Заготовки под еще два диалога

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import openNewPracticeDialogPlugin from "@/plugins/openNewPracticeDialogPlugin";
 import reviewPracticeDialogPlugin from "@/plugins/reviewPracticeDialogPlugin";
+import editPracticeDialogPlugin from "@/plugins/editPracticeDialogPlugin";
+import deletePracticeDialogPlugin from "@/plugins/deletePracticeDialogPlugin";
 
 
 Vue.config.productionTip = false
 
 Vue.use(openNewPracticeDialogPlugin);
 Vue.use(reviewPracticeDialogPlugin);
+Vue.use(editPracticeDialogPlugin);
+Vue.use(deletePracticeDialogPlugin);
 
 new Vue({
   store,
