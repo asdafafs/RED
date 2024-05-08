94a9d4d83178c49b5cfb2ea0d90a14d5406320b3
Anton <anton_vasilev00@mail.ru>
2024-05-08 22:00:15 +0300

    Вроде выравнял логотип

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container fluid>
-    <NavigationBar 
-      :drawer.sync="drawer" 
-      :role="role" 
-      :show-drawer="showDrawer" 
-      :student="discriminatorUser" 
-      :user="userName"
+    <NavigationBar
+        :drawer.sync="drawer"
+        :role="role"
+        :show-drawer="showDrawer"
+        :student="discriminatorUser"
+        :user="userName"
     />
-    <v-app-bar 
-      app 
-      density="compact" 
-      color="#1e1f22" 
-      class="appbar position_component app-bar-height"
+    <v-app-bar
+        app
+        density="compact"
+        color="#1e1f22"
+        class="appbar position_component app-bar-height"
     >
       <v-container
           v-if="showDrawer"
@@ -20,31 +20,31 @@
           style="max-height: 60px"
       >
         <v-row no-gutters class="pa-0 ma-0" style="max-height: 60px;flex-wrap: nowrap;">
-          <v-col cols=2>
+          <div style="width: min-content; padding-left: 24px">
             <DesktopLogo/>
-          </v-col>
+          </div>
           <v-col>
             <AppButtons
-              v-if="isDataLoaded"
-              :student="discriminatorUser"
+                v-if="isDataLoaded"
+                :student="discriminatorUser"
             />
           </v-col>
           <v-col cols="2">
             <div class="d-flex align-center" style="height: 100%; width: 90%">
               <UserProfile
-                class="mr-3 ml-3"
-                v-if="isDataLoaded"
-                :role="role"
-                :student="discriminatorUser"
-                :user="userName"
+                  class="mr-3 ml-3"
+                  v-if="isDataLoaded"
+                  :role="role"
+                  :student="discriminatorUser"
+                  :user="userName"
               />
             </div>
           </v-col>
         </v-row>
       </v-container>
       <MobileAppBar
-        v-else
-        :drawer.sync="drawer"
+          v-else
+          :drawer.sync="drawer"
       />
     </v-app-bar>
   </v-container>
